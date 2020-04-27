03730c262cf9c53325d6c14d6b19134194f17cd9
Yensey Murillo <yenseymurillo@gmail.com>
2020-04-27 15:01:27 -0500

    working on the pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,33 @@
-import React, {Component} from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
 
-import SearchPage from './components/Search';
-import SavePage from './components/Save';
+import SearchPage from './Pages/Search';
+import SavePage from './Pages/Saved';
 import Navigation from './components/Nav';
-// import Footer from './components/Footer';
-// import Jumbotron from './components/Jumbotron';
+import NoMatch from './Pages/noMatch';
+import Footer from './components/Footer';
+import Jumbotron from './components/Jumbotron';
+import Wrapper from './components/Wrapper';
 
 import './App.css';
 
 class App extends Component {
   render() {
-    return (      
+    return (
       <div>
         <BrowserRouter>
-         <div>
-           <Navigation />
-             <Switch>
-              <Route path="/" component={SearchPage} exact/>
-              <Route path="/save" component={SavePage}/>
-            </Switch>
-         </div> 
-       </BrowserRouter>
-      </div>
+          <Navigation />
+          <Jumbotron />
+          <div>
+            <Wrapper>
+              <Route path="/" component={SearchPage} exact />
+              <Route path="/save" component={SavePage} />
+              <Route path="/noMatch" component={NoMatch}/>
+            </Wrapper>  
+          </div>  
+        </BrowserRouter>
+        <Footer />
+    </div>
       );
     }
   }
